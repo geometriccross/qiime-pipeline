ed20ca6f5327621499b18071047f4a6d617a4417
geometriccross <geometriccross@LAPTOP.localdomain>
2025-01-04 23:58:51 +0900

    add boot strap playbook

---
+++ ansible/bootstrap.ps1
@@ -0,0 +1,98 @@
+#Requires -RunAsAdministrator
+
+param(
+    [string]$DistroUrl=$env:DISTRO_URL,
+    [string]$GitBranch=$env:GIT_BRANCH,
+    [string]$InstallDir=$env:INSTALL_DIR,
+    [string]$WslName=$env:WSL_NAME,
+    [string]$WslUsername=$env:WSL_USERNAME
+)
+
+if (-not "$InstallDir") {
+    $InstallDir = "${env:LOCALAPPDATA}\dev-bootstrap"
+}
+if (-not "$GitBranch") {
+    $GitBranch = "master"
+}
+if (-not "$DistroUrl") {
+    $DistroUrl = "https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-Container-Base.latest.x86_64.tar.xz"
+}
+if (-not "$WslName") {
+    $WslName = "rocky-9"
+}
+if (-not "$WslUsername") {
+    $WslUsername = $env:USERNAME.toLower()
+}
+
+Write-Debug "Check for WSL"
+if ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -ne "Enabled") {
+    Write-Debug "Enabling WSL"
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+    Write-Information "Restart required.  Please restart, then run this script again"
+    exit
+}
+else {
+    Write-Information "Checking for WSL update"
+    wsl --update
+}
+
+Write-Information "Set WSL default version to 2"
+wsl --set-default-version 2
+
+Write-Information "Import WSL distribution"
+$console = ([console]::OutputEncoding)
+[console]::OutputEncoding = New-Object System.Text.UnicodeEncoding
+$wslMatcher = (wsl --list --quiet | Select-String -Pattern "(?m)^$WslName$")
+[console]::OutputEncoding = $console
+if (-not $wslMatcher.Matches) {
+    Write-Information "WSL distribution $WslName not yet installed, installing"
+    $distroCache = "$InstallDir\distrocache"
+    $null = New-Item -Path "$distroCache" -Type Directory -Force
+
+    $wslDir = "${env:LOCALAPPDATA}\$WslName"
+    $null = New-Item -Path "$wslDir" -Type Directory -Force
+
+    $distroFile = "$distroCache\$WslName.tar.xz"
+    if (-not (Test-Path -Path "$distroFile" -PathType Leaf)) {
+        Start-BitsTransfer -Source "$DistroUrl" -Destination "$distroFile"
+    }
+
+    $wslVolume = "$wslDir\volume"
+    wsl --import "$WslName" "$wslVolume" "$distroFile"
+
+    Write-Information "Configure WSL $WslName"
+    wsl --distribution "$WslName" --user root --exec `
+        bash -c "
+            . /etc/os-release
+            if [[ `"`${ID_LIKE}`" =~ rhel ]]; then
+              dnf install --assumeyes systemd
+            fi
+    
+            cat > /etc/wsl.conf <<'EOF'
+[boot]
+systemd=true
+[user]
+default=$WslUsername
+EOF
+            chmod 0644 /etc/wsl.conf
+            "
+    # terminate to satisfy the 8 second rule (may need to switch to shutdown)
+    #   https://learn.microsoft.com/en-us/windows/wsl/wsl-config#the-8-second-rule
+    wsl --terminate "$WslName"
+}
+
+Write-Information "WSL user $WslUsername"
+wsl --distribution "$WslName" --user root --exec `
+    bash -c "grep '$WslUsername' /etc/passwd"
+if (-not $?) {
+    Write-Information "$WslUsername does not exist, creating..."
+    wsl --distribution "$WslName" --user root --exec `
+        bash -c "
+useradd --create-home '$WslUsername' --shell /bin/bash
+dnf install --assumeyes sudo
+mkdir --parents /etc/sudoers.d
+echo '$WslUsername ALL=(ALL) NOPASSWD:ALL' > '/etc/sudoers.d/$WslUsername'
+chmod 0440 '/etc/sudoers.d/$WslUsername'
+        "
+}
