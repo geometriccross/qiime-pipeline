f08c3639355bd918419cbffe20e3ad3ca38ba2f9
geometriccross <geometriccross@gmail.com>
2025-02-18 19:42:25 +0900

    file open

--- pipeline/create_maniffest.py
+++ pipeline/create_maniffest.py
@@ -1,1 +1,13 @@
 import csv
+from pathlib import Path
+
+bat_csv = Path("meta/bat_fleas.csv")
+cat_csv = Path("meta/cat_fleas.csv")
+lip_csv = Path("meta/lip_cervi.csv")
+mky_csv = Path("meta/mky_louse.csv")
+
+try:
+    bat = open(bat_csv, "r")
+    cat = open(cat_csv, "r")
+    lip = open(lip_csv, "r")
+    mky = open(mky_csv, "r")
