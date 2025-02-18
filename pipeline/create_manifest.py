ec5009734d58620c1add7eb6ed9f8510ee45715c
geometriccross <geometriccross@gmail.com>
2025-02-18 21:48:43 +0900

    manifestファイルを作成するスクリプトを作成

---
+++ pipeline/create_manifest.py
@@ -0,0 +1,27 @@
+from sys import argv
+from glob import glob
+from pathlib import Path
+
+root_dir = "."
+id_prefix = "id"
+if len(argv) > 1:
+    root_dir = argv[1]
+    id_prefix = argv[2]
+
+fastq_pathes = glob(root_dir + "/**/*gz", recursive=True)
+fastq_pathes = sorted(fastq_pathes, reverse=True)
+
+id_index = 1
+string = "sample-id, forward-absolute-filepath, reverse-absolute-filepath"
+while len(fastq_pathes) > 0:
+    forward = fastq_pathes.pop()
+    reverse = fastq_pathes.pop()
+
+    string += "\n"
+    string += id_prefix + id_index.__str__() + ", "
+    string += Path(forward).absolute().__str__() + ", "
+    string += Path(reverse).absolute().__str__() + ", "
+
+    id_index += 1
+
+print(string)
