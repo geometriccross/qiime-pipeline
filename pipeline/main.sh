a9e605309d6f6e50661df57b99809b785b12974c
geometriccross <geometriccross@LAPTOP.localdomain>
2025-02-25 16:53:25 +0900

    add option set

--- pipeline/main.sh
+++ pipeline/main.sh
@@ -9,5 +9,18 @@ export META_SOURCE="meta"
 export MANI="${OUT}/manifest.csv"
 export META="${OUT}/meta.csv"
 
+# https://unix.stackexchange.com/questions/706602/use-getopt-to-directly-retrieve-option-value
+while getopts m:c:o:d:x: OPT
+do
+	case $OPT in
+		o)	OUT=$2 ; shift;;
+		d)	DATA_SOURCE=$2 ; shift;;
+		m)	META_SOURCE=$OPTARG;;
+		c)	MANI=$OPTARG;;
+		x)	META=$OPTARG;;
+		*)	exit 1;;
+	esac
+done
+
 python pipeline/create_manifest.py "${DATA_SOURCE}" > "${MANI}"
 python pipeline/create_master_csv.py "${META_SOURCE}" > "${META}"
