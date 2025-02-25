690c6750b55bb62ded36e860acd902ab91ad9c0b
geometriccross <geometriccross@desktop.>
2025-02-25 22:47:02 +0900

    add args

---
+++ pipeline/extract_id.py
@@ -0,0 +1,31 @@
+from argparse import parser
+from textwrap import dedent
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "input_path",
+    help=dedent(
+        """
+        Path of data, metadata or manifest
+        """
+    )
+)
+parser.add_argument(
+    "target",
+    help=dedent(
+        """
+        Id that would extract id
+        """
+    )
+)
+parser.add_argument(
+    "-r",
+    "--reverse",
+    default=False,
+    type=bool,
+    help=dedent(
+        """
+        If you specify this TRUE, The data with the passed id removed is output
+        """
+        )
+)
