621d8f180f5a1c6a15091ebefa7c2cf68113e33c
geometriccross <geometriccross@LAPTOP.localdomain>
2025-02-25 16:50:40 +0900

    add env car set

--- pipeline/main.sh
+++ pipeline/main.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+tmp="out/$(sha256sum <(date) | cut -d ' ' -f 1)"
+export OUT="${tmp}"
+unset tmp
+
+export DATA_SOURCE="fastq"
+export META_SOURCE="meta"
+export MANI="${OUT}/manifest.csv"
+export META="${OUT}/meta.csv"
