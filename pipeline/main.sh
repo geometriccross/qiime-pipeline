59bb8f416acf7e07df2185e4c881f69c55faf8b3
geometriccross <geometriccross@desktop.>
2025-02-26 20:55:33 +0900

    add sampling depth option

--- pipeline/main.sh
+++ pipeline/main.sh
@@ -10,7 +10,7 @@ export MANI="${OUT}/manifest"
 export META="${OUT}/meta"
 
 # https://unix.stackexchange.com/questions/706602/use-getopt-to-directly-retrieve-option-value
-while getopts m:c:o:d:x: OPT
+while getopts m:c:o:d:x:s: OPT
 do
 	case $OPT in
 		o)	OUT=$2 ; shift;;
@@ -18,6 +18,7 @@ do
 		m)	META_SOURCE=$OPTARG;;
 		c)	MANI=$OPTARG;;
 		x)	META=$OPTARG;;
+		s)	SAMPLING_DEPTH=$OPTARG;;
 		*)	exit 1;;
 	esac
 done
@@ -25,5 +26,10 @@ done
 mkdir -p "${OUT}"
 python pipeline/create_manifest.py "${DATA_SOURCE}" > "${MANI}"
 python pipeline/create_master.py "${META_SOURCE}" > "${META}"
+
+if [[ -z "${SAMPLING_DEPTH}" ]]; then
+	./pipeline/rarefaction.sh | xargs ./pipeline/view.sh
+	exit 0
+fi
 
 ./pipeline/pipeline.sh
