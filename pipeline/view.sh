89ff842b4a64e87dfa86ec83a3d50a3ed9748b56
geometriccross <geometriccross@LAPTOP.localdomain>
2025-02-26 14:43:08 +0900

    fix space

--- pipeline/view.sh
+++ pipeline/view.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ -z "${WSL_DISTRO_NAME}" ]]; then
-	echo Not in WSL > /dev/stderr
+	echo Not in WSL >/dev/stderr
 fi
 
 if [[ -z "${1}" ]]; then
-	echo No input > /dev/stderr
+	echo No input >/dev/stderr
 fi
 
 if [[ -z "${BROWSER}" ]]; then
-	echo ENV BROWSER not set > /dev/stderr
+	echo ENV BROWSER not set >/dev/stderr
 fi
