7a7c3146394a917a2a705c81bf2498bb5d70a773
geometriccross <geometriccross@desktop.>
2025-02-28 01:26:19 +0900

    add db download

--- pipeline/main.sh
+++ pipeline/main.sh
@@ -6,16 +6,18 @@ unset tmp
 
 export FASTQ="fastq"
 export META_SOURCE="meta"
+export DB="db/classifier.qza"
 export MANI="${OUT}/manifest"
 export META="${OUT}/meta"
 
 # https://unix.stackexchange.com/questions/706602/use-getopt-to-directly-retrieve-option-value
-while getopts m:c:o:f:x:s: OPT
+while getopts m:c:o:f:x:s:d: OPT
 do
 	case $OPT in
 		o)	OUT=$OPTARG;;
 		f)	FASTQ=$OPTARG;;
 		m)	META_SOURCE=$OPTARG;;
+		d)	DB=$OPTARG;;
 		c)	MANI=$OPTARG;;
 		x)	META=$OPTARG;;
 		s)	SAMPLING_DEPTH=$OPTARG;;
@@ -27,9 +29,15 @@ mkdir -p "${OUT}"
 python pipeline/create_manifest.py "${FASTQ}" > "${MANI}"
 python pipeline/create_master.py "${META_SOURCE}" > "${META}"
 
+if [ ! -f "$DB" ]; then
+	dirname "${DB}" | xargs mkdir -p
+	echo "$DB" | xargs wget "https://bwsyncandshare.kit.edu/s/zK5zjAbsTFQRpbo/download?path=%2Fq2_24-10%2FSilva138_2&files=classifier-V34.qza" -O
+fi
+
 # if variable was not set
 if [ -z ${SAMPLING_DEPTH+x} ]; then
 	./pipeline/rarefaction.sh | xargs ./pipeline/view.sh
 else
 	./pipeline/pipeline.sh
 fi
+
