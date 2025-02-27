4b5d532ffe82104ad38ab02049a5e86498d32ce8
geometriccross <geometriccross@desktop.>
2025-02-28 00:57:06 +0900

    change variable name

--- pipeline/main.sh
+++ pipeline/main.sh
@@ -4,17 +4,17 @@ tmp="out/$(date +%h%d-%H-%M)_$(tr -dc 0-9A-Za-z < /dev/urandom | fold -w 10 | he
 export OUT="${tmp}"
 unset tmp
 
-export DATA_SOURCE="fastq"
+export FASTQ="fastq"
 export META_SOURCE="meta"
 export MANI="${OUT}/manifest"
 export META="${OUT}/meta"
 
 # https://unix.stackexchange.com/questions/706602/use-getopt-to-directly-retrieve-option-value
-while getopts m:c:o:d:x:s: OPT
+while getopts m:c:o:f:x:s: OPT
 do
 	case $OPT in
 		o)	OUT=$2 ; shift;;
-		d)	DATA_SOURCE=$2 ; shift;;
+		f)	FASTQ=$2 ; shift;;
 		m)	META_SOURCE=$OPTARG;;
 		c)	MANI=$OPTARG;;
 		x)	META=$OPTARG;;
@@ -24,7 +24,7 @@ do
 done
 
 mkdir -p "${OUT}"
-python pipeline/create_manifest.py "${DATA_SOURCE}" > "${MANI}"
+python pipeline/create_manifest.py "${FASTQ}" > "${MANI}"
 python pipeline/create_master.py "${META_SOURCE}" > "${META}"
 
 # if variable was not set
