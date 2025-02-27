39a9ad06520ec58b86a1527feef6d23024f02902
geometriccross <geometriccross@desktop.>
2025-02-27 21:23:04 +0900

    remove comment

--- pipeline/rarefaction.sh
+++ pipeline/rarefaction.sh
@@ -11,11 +11,6 @@ qiime tools import \
 	--input-path "${MANI}" \
 	--output-path "${RAREFACTION_DIR}/paired_end_demux.qza" >/dev/null
 
-# qiime demux summarize \
-# 	--quiet \
-# 	--i-data "${RAREFACTION_DIR}/paired_end_demux.qza" \
-# 	--o-visualization "${RAREFACTION_DIR}/paired_end_demux.qzv"
-
 qiime dada2 denoise-paired \
 	--quiet \
 	--i-demultiplexed-seqs "${RAREFACTION_DIR}/paired_end_demux.qza" \
@@ -27,19 +22,6 @@ qiime dada2 denoise-paired \
 	--o-table "${RAREFACTION_DIR}/denoised_table.qza" \
 	--o-representative-sequences "${RAREFACTION_DIR}/denoised_seq.qza" \
 	--o-denoising-stats "${RAREFACTION_DIR}/denoised_stats.qza"
-
-# qiime feature-table summarize \
-# 	--i-table first/denoise/table.qza \
-# 	--o-visualization first/denoise/table.qzv \
-# 	--m-sample-metadata-file source/metadata/bat-fleas-metadata.tsv &
-
-# qiime feature-table tabulate-seqs \
-# 	--i-data first/denoise/representative_sequences.qza \
-# 	--o-visualization first/denoise/representative_sequences.qzv &
-
-# qiime metadata tabulate \
-# 	--m-input-file first/denoise/denoising_stats.qza \
-# 	--o-visualization first/denoise/denoising_stats.qzv &
 
 qiime phylogeny align-to-tree-mafft-fasttree \
 	--quiet \
