f37583ec55095bb0c2281a8c1a9fbf4187d32d2c
pc1 <pc1@VM01519K0050D>
2025-03-05 17:53:09 +0900

    metadataとmanifestを作成する部分

--- pipeline/create_Mfiles.py
+++ pipeline/create_Mfiles.py
@@ -26,9 +26,42 @@ parser.add_argument(
     ).strip()
 )
 
+id_prefix = parser.parse_args().id_prefix,
 master_header = data_list[0]["meta"].readline().replace("\n", "")
 master_header = [
-    parser.parse_args().id_prefix,
+    id_prefix,
     *master_header.replace("#", "").replace("SampleID", "RawID").split(",")
 ]
 data_list[0]["meta"].seek(0)
+
+id_index = 1
+mani_result = "sample-id\tforward-absolute-filepath\treverse-absolute-filepath"
+master_list = []
+for pair in data_list:
+    pre_indexer = id_index
+    # create manifest
+    fastq_pathes = glob(pair["fastq"], "/**/*gz", recursive=True)
+    while len(fastq_pathes) > 0:
+        forward = fastq_pathes.pop()
+        reverse = fastq_pathes.pop()
+
+        mani_result += "\n"
+        mani_result += id_prefix + id_index.__str__() + "\t"
+        mani_result += Path(forward).absolute().__str__() + "\t"
+        mani_result += Path(reverse).absolute().__str__() + "\t"
+
+        id_index += 1
+
+    # reset index
+    id_index = pre_indexer
+    # create csv
+    with open(Path(pair["meta"]).absolute(), "r") as f:
+        reader = csv.reader(f)
+        header_removed = [row for row in reader][1:]
+        for row in header_removed:
+            master_list.append([
+                id_prefix + id_index.__str__(),
+                *row
+            ])
+            id_index += 1
+
