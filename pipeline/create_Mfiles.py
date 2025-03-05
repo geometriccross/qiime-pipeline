f6e0d007c99fb352866dad491d095f0b37f26af7
pc1 <pc1@VM01519K0050D>
2025-03-05 17:35:33 +0900

    対応表を辞書を含むリストに変換

--- pipeline/create_Mfiles.py
+++ pipeline/create_Mfiles.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-data_map = {
-    "meta/bat_fleas.csv": "fastq/batfleas",
-    "meta/cat_fleas.csv": "fastq/catfleas",
-    "meta/lip_cervi.csv": "fastq/sk",
-    "meta/mky_louse.csv": "fastq/monkeylice"
-}
+data_list = [
+    {"meta": "meta/bat_fleas.csv", "fastq": "fastq/batfleas"},
+    {"meta": "meta/cat_fleas.csv", "fastq": "fastq/catfleas"},
+    {"meta": "meta/lip_cervi.csv", "fastq": "fastq/sk"},
+    {"meta": "meta/mky_louse.csv", "fastq": "fastq/monkeylice"}
+]
