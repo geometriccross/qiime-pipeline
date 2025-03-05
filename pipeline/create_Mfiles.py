fd0f393ae26dd80b777bd8fc8e4815865dd56430
pc1 <pc1@VM01519K0050D>
2025-03-05 17:40:51 +0900

    ヘッダの取得部分を作成

--- pipeline/create_Mfiles.py
+++ pipeline/create_Mfiles.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 
+import csv
+import argparse
+from textwrap import dedent
+from glob import glob
+from pathlib import Path
+
 data_list = [
     {"meta": "meta/bat_fleas.csv", "fastq": "fastq/batfleas"},
     {"meta": "meta/cat_fleas.csv", "fastq": "fastq/catfleas"},
     {"meta": "meta/lip_cervi.csv", "fastq": "fastq/sk"},
     {"meta": "meta/mky_louse.csv", "fastq": "fastq/monkeylice"}
 ]
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-p",
+    "--id-prefix",
+    default="id",
+    help=dedent(
+        """
+        set prefix id of each sample like this [REPLACE THIS] + numeric
+        (default : %(default)s))
+        """
+    ).strip()
+)
+
+master_header = data_list[0]["meta"].readline().replace("\n", "")
+master_header = [
+    parser.parse_args().id_prefix,
+    *master_header.replace("#", "").replace("SampleID", "RawID").split(",")
+]
+data_list[0]["meta"].seek(0)
