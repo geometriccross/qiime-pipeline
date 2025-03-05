2e979e6c68aa0d2a2c6b4716dbcd8292c859bd04
geometriccross <geometriccross@desktop.>
2025-03-06 00:42:09 +0900

    metadataのidを基準に、fastqを探すようにした

--- pipeline/create_Mfiles.py
+++ pipeline/create_Mfiles.py
@@ -7,6 +7,28 @@ from textwrap import dedent
 from glob import glob
 from pathlib import Path, PurePath
 
+
+# queryで渡されたidを持つfastqファイルを返す
+def search_fastq(q: str, data: list[str]) -> list[str]:
+    correct = []
+    for d in data:
+        file_name = PurePath(d).name
+        if re.match(f"^{q.upper()}_", file_name.upper()):
+            correct.append(d)
+
+    # R1, R2以外に名前がかぶっているfastqファイルがあったら
+    if len(correct) > 2:
+        raise SyntaxError(dedent(f"""
+            同名のファイルが3つ以上存在しています。\n
+            {correct}
+        """))
+
+    fwd = list(filter(lambda s: s.__contains__("_R1_"), correct))
+    rvs = list(filter(lambda s: s.__contains__("_R2_"), correct))
+
+    return [*fwd, *rvs]
+
+
 data_list = [
     {"meta": "meta/bat_fleas.csv", "fastq": "fastq/batfleas"},
     {"meta": "meta/cat_fleas.csv", "fastq": "fastq/catfleas"},
@@ -49,9 +71,8 @@ out_meta = parser.parse_args().out_meta
 out_mani = parser.parse_args().out_mani
 
 master_list = []
-# metadataのヘッダーを取得
-tmp_meta = data_list[0]["meta"]
-with Path(tmp_meta).open() as f:
+# 適当なmetadataのヘッダーを取得
+with Path(data_list[0]["meta"]).open() as f:
     header_str = f.readline().replace("\n", "")
     # csv writerowsで正常に書き込むためには二次元配列でなければならない
     master_list = [[
@@ -66,39 +87,27 @@ mani_result.append([
     "reverse-absolute-filepath"
 ])
 
-mani_id = 1
-meta_id = 1
+id_index = 1
 for pair in data_list:
-    # このようなファイル名のとき: Ca-fle21_S1_L001_R2_001.fastq.gz
-    # 最初のアンダースコアまでで値を区切り、その最初の値に含まれる数字でソートする
-    fastq_pathes = sorted(
-        glob(pair["fastq"] + "/**/*gz", recursive=True),
-        key=lambda s: int(re.sub(r'\D', "", PurePath(s).name.split('_')[0])),
-        reverse=True
-    )
-
-    while len(fastq_pathes) > 0:
-        forward = fastq_pathes.pop()
-        reverse = fastq_pathes.pop()
-
-        row = []
-        row.append(id_prefix + mani_id.__str__())
-        row.append(Path(forward).absolute().__str__())
-        row.append(Path(reverse).absolute().__str__())
-        mani_result.append(row)
-
-        mani_id += 1
-
-    # create csv
+    fastq_pathes = glob(pair["fastq"] + "/**/*gz", recursive=True)
     with open(Path(pair["meta"]).absolute(), "r") as f:
         reader = csv.reader(f)
         header_removed = [row for row in reader][1:]
         for row in header_removed:
+            # master csv
             master_list.append([
-                id_prefix + meta_id.__str__(),
+                id_prefix + id_index.__str__(),
                 *row
             ])
-            meta_id += 1
+
+            # manifest
+            # 1列目にあるRawIDから対応するfastqファイルを取得
+            mani_result.append([
+                id_prefix + id_index.__str__(),
+                *search_fastq(row[0], fastq_pathes)
+            ])
+
+            id_index += 1
 
 with open(out_meta, "w") as f:
     writer = csv.writer(f, delimiter="\t")
