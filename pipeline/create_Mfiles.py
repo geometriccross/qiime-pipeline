14ae47c8ea283bde2913d76cb77abdc8735c12bc
pc1 <pc1@VM01519K0050D>
2025-03-05 17:25:26 +0900

    対応表を追加

--- pipeline/create_Mfiles.py
+++ pipeline/create_Mfiles.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+
+data_map = {
+    "meta/bat_fleas.csv": "fastq/batfleas",
+    "meta/cat_fleas.csv": "fastq/catfleas",
+    "meta/lip_cervi.csv": "fastq/sk",
+    "meta/mky_louse.csv": "fastq/monkeylice"
+}
