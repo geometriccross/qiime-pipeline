4d59a9c17d4498b82160bbb8132e9a3592b2cf66
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-04 18:12:01 +0900

    change variable name

--- pipeline/main.sh
+++ pipeline/main.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # default value
-OUT="/out"
-DB="/db/classifier.qza"
-MANI="${OUT}/manifest"
-META="${OUT}/meta"
+HOST_OUT="out/"
+HOST_DB="db/classifier.qza"
+HOST_MANI="${OUT}/manifest"
+HOST_META="${OUT}/meta"
 
 # https://unix.stackexchange.com/questions/706602/use-getopt-to-directly-retrieve-option-value
 while getopts m:c:o:f:x:s:d: OPT; do
 	case $OPT in
-	o) OUT=$OPTARG ;;
-	d) DB=$OPTARG ;;
-	c) MANI=$OPTARG ;;
-	x) META=$OPTARG ;;
+	o) HOST_OUT=$OPTARG ;;
+	d) HOST_DB=$OPTARG ;;
+	c) HOST_MANI=$OPTARG ;;
+	x) HOST_META=$OPTARG ;;
 	s) SAMPLING_DEPTH=$OPTARG ;;
 	*) exit 1 ;;
 	esac
@@ -20,14 +20,14 @@ done
 
 docker build . -t qiime
 
-mkdir -p "$OUT"
-./pipeline/create_Mfiles.py --id-prefix id --out-meta "$META" --out-mani "$MANI"
-./pipeline/check_manifest.py "$MANI"
+mkdir -p "$HOST_OUT"
+./pipeline/create_Mfiles.py --id-prefix id --out-meta "$HOST_META" --out-mani "$HOST_MANI"
+./pipeline/check_manifest.py "$HOST_MANI"
 
-if [ ! -f "$DB" ]; then
-	dirname "$DB" | xargs mkdir -p
+if [ ! -f "$HOST_DB" ]; then
+	dirname "$HOST_DB" | xargs mkdir -p
 	docker container run --rm qiime /pipeline/db.sh | \
-		xargs -I FILE docker cp qiime:FILE "$(realpath "$DB" | dirname)"
+		xargs -I FILE docker cp qiime:FILE "$(realpath "$HOST_DB" | dirname)"
 fi
 
 # if variable was not set
