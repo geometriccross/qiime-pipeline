196c32719ccb393e5f34a87865559175f22f03f0
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-04 15:47:56 +0900

    use cd insted that specify a path

--- pipeline/pipeline.sh
+++ pipeline/pipeline.sh
@@ -4,12 +4,13 @@ set -e -x
 
 PRE="${OUT}/pre_$(tr -dc 0-9A-Za-z </dev/urandom | fold -w 10 | head -1)"
 mkdir -p "${PRE}"
+cd "$PRE" || exit 1
 
 qiime tools import \
 	--type 'SampleData[PairedEndSequencesWithQuality]' \
 	--input-format PairedEndFastqManifestPhred33V2 \
 	--input-path "${MANI}" \
-	--output-path "${PRE}/paired_end_demux.qza" >/dev/null
+	--output-path paired_end_demux.qza >/dev/null
 
 qiime dada2 denoise-paired \
 	--quiet \
@@ -18,101 +19,106 @@ qiime dada2 denoise-paired \
 	--p-trim-left-r 21 \
 	--p-trunc-len-f 250 \
 	--p-trunc-len-r 250 \
-	--i-demultiplexed-seqs "${PRE}/paired_end_demux.qza" \
-	--o-table "${PRE}/denoised_table.qza" \
-	--o-representative-sequences "${PRE}/denoised_seq.qza" \
-	--o-denoising-stats "${PRE}/denoised_stats.qza"
+	--i-demultiplexed-seqs paired_end_demux.qza \
+	--o-table denoised_table.qza \
+	--o-representative-sequences denoised_seq.qza \
+	--o-denoising-stats denoised_stats.qza
 
 qiime feature-table filter-samples \
 	--quiet \
 	--p-min-frequency "${SAMPLING_DEPTH}" \
-	--i-table "${PRE}/denoised_table.qza" \
-	--o-filtered-table "${PRE}/filtered_table.qza"
+	--i-table denoised_table.qza \
+	--o-filtered-table filtered_table.qza
 
 qiime feature-table filter-seqs \
 	--quiet \
-	--i-data "${PRE}/denoised_seq.qza" \
-	--i-table "${PRE}/filtered_table.qza" \
-	--o-filtered-data "${PRE}/filtered_seq.qza"
+	--i-data denoised_seq.qza \
+	--i-table filtered_table.qza \
+	--o-filtered-data filtered_seq.qza
 
 qiime feature-classifier classify-sklearn \
 	--quiet \
 	--i-classifier "${DB}" \
-	--i-reads "${PRE}/filtered_seq.qza" \
-	--o-classification "${PRE}/classification.qza"
+	--i-reads filtered_seq.qza \
+	--o-classification classification.qza
 
 qiime taxa filter-table \
 	--quiet \
 	--p-exclude mitochondria,cyanobacteria \
-	--i-table "${PRE}/filtered_table.qza" \
-	--i-taxonomy "${PRE}/classification.qza" \
-	--o-filtered-table "${PRE}/common_biology_free_table.qza"
+	--i-table filtered_table.qza \
+	--i-taxonomy classification.qza \
+	--o-filtered-table common_biology_free_table.qza
 
 qiime taxa filter-seqs \
 	--quiet \
 	--p-exclude mitochondria,cyanobacteria \
-	--i-sequences "${PRE}/filtered_seq.qza" \
-	--i-taxonomy "${PRE}/classification.qza" \
-	--o-filtered-sequences "${PRE}/common_biology_free_seq.qza"
+	--i-sequences filtered_seq.qza \
+	--i-taxonomy classification.qza \
+	--o-filtered-sequences common_biology_free_seq.qza
 
 qiime phylogeny align-to-tree-mafft-fasttree \
 	--quiet \
-	--i-sequences "${PRE}/common_biology_free_seq.qza" \
-	--o-alignment "${PRE}/common_biology_free_aligned-rep-seqs.qza" \
-	--o-masked-alignment "${PRE}/common_biology_free_masked-aligned-rep-seqs.qza" \
-	--o-tree "${PRE}/common_biology_free_unrooted-tree.qza" \
-	--o-rooted-tree "${PRE}/common_biology_free_rooted-tree.qza"
+	--i-sequences common_biology_free_seq.qza \
+	--o-alignment common_biology_free_aligned-rep-seqs.qza \
+	--o-masked-alignment common_biology_free_masked-aligned-rep-seqs.qza \
+	--o-tree common_biology_free_unrooted-tree.qza \
+	--o-rooted-tree common_biology_free_rooted-tree.qza
 
 qiime feature-classifier classify-sklearn \
 	--quiet \
 	--i-classifier "${DB}" \
-	--i-reads "${PRE}/common_biology_free_seq.qza" \
-	--o-classification "${PRE}/common_biology_free_classification.qza"
+	--i-reads common_biology_free_seq.qza \
+	--o-classification common_biology_free_classification.qza
 
 qiime taxa barplot \
 	--quiet \
-	--i-table "${PRE}/common_biology_free_table.qza" \
-	--i-taxonomy "${PRE}/common_biology_free_classification.qza" \
+	--i-table common_biology_free_table.qza \
+	--i-taxonomy common_biology_free_classification.qza \
 	--m-metadata-file "${META}" \
-	--o-visualization "${PRE}/taxa-bar-plots.qzv"
+	--o-visualization taxa-bar-plots.qzv
 
 # ./pipeline/view.sh "${PRE}/taxa-bar-plots.qzv"
 
 CORE="${OUT}/core_$(tr -dc 0-9A-Za-z </dev/urandom | fold -w 10 | head -1)"
+mkdir "$CORE"
+cd "$CORE" || exit 1
 
 qiime diversity core-metrics-phylogenetic \
 	--quiet \
 	--m-metadata-file "${META}" \
 	--p-sampling-depth "${SAMPLING_DEPTH}" \
-	--i-phylogeny "${PRE}/common_biology_free_rooted-tree.qza" \
-	--i-table "${PRE}/common_biology_free_table.qza" \
+	--i-phylogeny common_biology_free_rooted-tree.qza \
+	--i-table common_biology_free_table.qza \
 	--output-dir "${CORE}"
 
 qiime metadata tabulate \
-	--m-input-file "${CORE}/faith_pd_vector.qza" \
-	--o-visualization "${CORE}/faith_pd_vector.qzv"
+	--m-input-file faith_pd_vector.qza \
+	--o-visualization faith_pd_vector.qzv
 
 # ./pipeline/view.sh "${CORE}/faith_pd_vector.qzv"
 
 ALPHA="${OUT}/alpha_$(tr -dc 0-9A-Za-z </dev/urandom | fold -w 10 | head -1)"
 mkdir -p "${ALPHA}"
-qiime diversity alpha-group-significance \
-	--m-metadata-file "${META}" \
-	--i-alpha-diversity "${CORE}/shannon_vector.qza" \
-	--o-visualization "${ALPHA}/shannon_vector.qzv"
+cd "$ALPHA" || exit 1
 
 qiime diversity alpha-group-significance \
 	--m-metadata-file "${META}" \
-	--i-alpha-diversity "${CORE}/faith_pd_vector.qza" \
-	--o-visualization "${ALPHA}/faith_pd_vector.qzv"
+	--i-alpha-diversity shannon_vector.qza \
+	--o-visualization shannon_vector.qzv
 
 qiime diversity alpha-group-significance \
 	--m-metadata-file "${META}" \
-	--i-alpha-diversity "${CORE}/observed_features_vector.qza" \
-	--o-visualization "${ALPHA}/observed_features_vector.qzv"
+	--i-alpha-diversity faith_pd_vector.qza \
+	--o-visualization faith_pd_vector.qzv
+
+qiime diversity alpha-group-significance \
+	--m-metadata-file "${META}" \
+	--i-alpha-diversity observed_features_vector.qza \
+	--o-visualization observed_features_vector.qzv
 
 BETA="${OUT}/beta_$(tr -dc 0-9A-Za-z </dev/urandom | fold -w 10 | head -1)"
 mkdir -p "${BETA}"
+cd "$BETA" || exit 1
 
 col=("Host" "Species" "Location" "SampleGender")
 for item in "${col[@]}"; do
@@ -120,8 +126,8 @@ for item in "${col[@]}"; do
 		--p-pairwise \
 		--m-metadata-file "${META}" \
 		--m-metadata-column "${item}" \
-		--i-distance-matrix "${CORE}/weighted_unifrac_distance_matrix.qza" \
-		--o-visualization "${BETA}/weighted-unifrac-distance-matrix-${item}.qzv"
+		--i-distance-matrix weighted_unifrac_distance_matrix.qza \
+		--o-visualization weighted-unifrac-distance-matrix-"${item}".qzv
 done
 
 # # -----------------------------------------------------------------------------------------------------------
