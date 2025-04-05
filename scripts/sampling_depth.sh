c36d12cbf4a8cb11403f605d71bd9d30998ec4d1
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-05 17:28:30 +0900

    add arg

--- scripts/sampling_depth.sh
+++ scripts/sampling_depth.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+while getopts m:c:o:f:x:s:d: OPT; do
+	case $OPT in
+	c) MANI=$OPTARG ;;
+	x) META=$OPTARG ;;
+	*) 
+		echo "Arguments are required" > /dev/err
+		exit 1 ;;
+	esac
+done
+
 batch_id=sampling_depth_$(idgen.sh)
 docker build . -t "$batch_id"
 
@@ -7,8 +17,8 @@ mkdir -p "out/$batch_id"
 docker run --rm sampling_depth /scripts/pipeline/rarefaction.sh \
 	--mount -type=bind "$batch_id",src="$(pwd)"/fastq,dst=/fastq,readonly \
 	--mount -type=bind "$batch_id",src="$(pwd)"/meta,dst=/meta,readonly \
-	-c "$HOST_MANI" \
-	-x "$HOST_META" |
+	-c "$MANI" \
+	-x "$META" |
 	xargs -I FILE docker cp sampling_depth:FILE "out/$batch_id/"
 
 	find "out/$batch_id/" -type f -name ".qzv" | \
