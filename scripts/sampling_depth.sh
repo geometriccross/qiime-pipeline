248bb3bd2534293beffd0a74b107ab53141f280c
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-05 19:31:03 +0900

    run container in background

--- scripts/sampling_depth.sh
+++ scripts/sampling_depth.sh
@@ -6,15 +6,10 @@ mkdir -p "out/$batch_id"
 
 batch_id=sampling_depth_$(./scripts/idgen.sh)
 docker build . -t "$batch_id"
-if docker run --name "$batch_id" \
+docker run -dit --name "$batch_id" \
 	--mount type=bind,src="$(pwd)"/fastq,dst=/fastq,readonly \
 	--mount type=bind,src="$(pwd)"/meta,dst=/meta,readonly \
-	"$batch_id" bash -c "$cmd"
-then
-	cat - | xargs -I FILE docker cp "$batch_id":FILE "out/$batch_id/"
-	find "out/$batch_id/" -type f -name ".qzv" | \
-		xargs -0 ./scripts/view.sh  # run in the host
-fi
+	"$batch_id" bash
 
 # docker execではentorypointを経由せず直接コマンドを実行するためbase環境が認識されない
 # そのためexecを使用する際にはbaseを認識させなければならない
