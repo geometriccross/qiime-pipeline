88ace14ad513c6e9a999f3bdf7814aae52a01294
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-05 18:30:28 +0900

    cmdを分けるようにした。また、処理の最後でimageとcontainerを削除するように

--- scripts/sampling_depth.sh
+++ scripts/sampling_depth.sh
@@ -2,17 +2,31 @@
 
 set -ex
 
-batch_id=sampling_depth_$(./scripts/idgen.sh)
-docker build . -t "$batch_id"
+cmd="$(cat - << EOF
+	touch /meta /mani
+	/scripts/create_Mfiles.py --id-prefix id --out-meta /meta --out-mani /mani
+	/scripts/check_manifest.py /mani
+	
+	 # /scripts/pipeline/rarefaction.sh -c /mani -x /meta
+EOF
+)"
+
 
 mkdir -p "out/$batch_id"
-docker run --rm sampling_depth /scripts/pipeline/rarefaction.sh \
-	--mount -type=bind "$batch_id",src="$(pwd)"/fastq,dst=/fastq,readonly \
-	--mount -type=bind "$batch_id",src="$(pwd)"/meta,dst=/meta,readonly \
-	-c "$MANI" \
-	-x "$META" |
-	xargs -I FILE docker cp sampling_depth:FILE "out/$batch_id/"
 
+batch_id=sampling_depth_$(./scripts/idgen.sh)
+docker build . -t "$batch_id"
+if docker run --name "$batch_id" \
+	--mount type=bind,src="$(pwd)"/fastq,dst=/fastq,readonly \
+	--mount type=bind,src="$(pwd)"/meta,dst=/meta,readonly \
+	"$batch_id" bash -c "$cmd"
+then
+	cat - | xargs -I FILE docker cp "$batch_id":FILE "out/$batch_id/"
 	find "out/$batch_id/" -type f -name ".qzv" | \
-		xargs -0 ./scripts/pipeline/view.sh  # run in the host
+		xargs -0 ./scripts/view.sh  # run in the host
+fi
+
+docker rm "$batch_id"
+docker rmi "$batch_id"
+
 
