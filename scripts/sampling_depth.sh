6a3da80b94e582eec0d5718ecea952c1b4ae5feb
geometriccross <geometriccross@desktop.>
2025-04-05 23:22:57 +0900

    fix a -name and add option

--- scripts/sampling_depth.sh
+++ scripts/sampling_depth.sh
@@ -42,7 +42,7 @@ de '/scripts/pipeline/rarefaction.sh -c /tmp/mani -x /tmp/meta' |
 	tr -d "'\r\n" | # remove CRLR and single quote
 	xargs -I FILE docker cp "$batch_id":FILE "out/$batch_id/"
 
-find "out/$batch_id/" -type f -name ".qzv" |
+find "out/$batch_id/" -type f -name "*.qzv" -print0 |
 	xargs -0 ./scripts/view.sh # run in the host
 
 docker stop "$batch_id"
