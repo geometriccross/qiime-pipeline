e6c8abdaf0dde82f8869618001926bc6539f7cdd
geometriccross <geometriccross@desktop.>
2025-04-07 00:24:37 +0900

    change path specification

--- scripts/metagenome.sh
+++ scripts/metagenome.sh
@@ -4,4 +4,5 @@
 
 de "/scripts/pipeline/taxonomy.sh -s $SAMPLING_DEPTH"
 
-docker cp "$batch_id":/tmp/out "out/$batch_id/"
+# /tmp/out/.のように最後に.をつけることで、指定したディレクトリの中身を直接コピーすることとなる
+docker cp "$batch_id":/tmp/out/. "out/$batch_id/"
