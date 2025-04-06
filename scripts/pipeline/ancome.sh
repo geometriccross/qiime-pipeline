3559872c4ec5d7c1f1454ac4489474bc7bcf8428
geometriccross <geometriccross@desktop.>
2025-04-07 02:51:16 +0900

    add ancome script

--- scripts/pipeline/ancome.sh
+++ scripts/pipeline/ancome.sh
@@ -6,72 +6,24 @@ if [[ ! -e /.dockerenv ]]; then
 	exit 1
 fi
 
-# # -----------------------------------------------------------------------------------------------------------
-# #!/bin/bash
-# # 【はじめに】
-# # 2次解析で生成したデータをもとにHeatmapを作成していきます。
-# #
-# # 【コマンドライン 3次解析】
-# # 各サンプルのOTUとその数をTaxonomic levelごとに確認していきます。
-# # 以下のコマンドをまとめてコピペし、実行してください。
-#
-# set -e -x
-#
-# mkdir third
-#
-# for level in 6 4 3; do
-# 	echo level "$level", start
-#
-# 	mkdir third/collapse"$level"
-# 	path=third/collapse"$level"
-#
-# 	if qiime taxa collapse \
-# 		--p-level "$level" \
-# 		--i-table second/filtered/filtered-table.qza \
-# 		--i-taxonomy second/taxonomy/classification.qza \
-# 		--o-collapsed-table "$path"/collapse"$level"-table.qza &&
-# 		qiime composition add-pseudocount \
-# 			--i-table third/collapse"$level"/collapse"$level"-table.qza \
-# 			--o-composition-table "$path"/collapse"$level"-composition.qza; then
-#
-# 		qiime feature-table heatmap \
-# 			--i-table "$path"/collapse"$level"-table.qza \
-# 			--o-visualization "$path"/heatmap"$level"-HostGender.qzv \
-# 			--m-metadata-file source/metadata/bat-fleas-filtered-metadata.tsv \
-# 			--m-metadata-column HostGender &
-#
-# 		qiime feature-table heatmap \
-# 			--i-table "$path"/collapse"$level"-table.qza \
-# 			--o-visualization "$path"/heatmap"$level"-HostIDNo.qzv \
-# 			--m-metadata-file source/metadata/bat-fleas-filtered-metadata.tsv \
-# 			--m-metadata-column HostIDNo &
-#
-# 		qiime tools export --input-path "$path"/collapse"$level"-composition.qza --output-path "$path"/export &&
-# 			mv "$path"/export/feature-table.biom $path"/feature-table.biom"
-#
-# 		biom convert -i "$path"/feature-table.biom -o "$path"/level"$level"-otu-taxonomic.tsv --to-tsv
-# 	fi
-#
-# 	echo -e "level $level, finish\n"
-# done
-#
-# # これですべての解析が終了しました。
-# # 最後に今まで生成したすべてのファイルに、解析が終了した日付を付けます。
-# # 次のコマンドを実行してください。
-#
-# wait
-#
-# find first second third -print0 -name "*.qz[av]" |
-# 	xargs -0 rename.ul --verbose -- ".qz" "-$(date "+%y%m%d").qz"
-while getopts t: OPT
-do
-	case $OPT in
-		t)	
-			TABLE=$OPTARG
-			;;
-		*)	
-			exit 1
-			;;
-	esac
-done
+BASIC=/tmp/out/basic
+ANCOME="/tmp/out/ancome"
+mkdir -p "$ANCOME"
+cd "$ANCOME" || exit 1
 
+# taxonomy解析で生成したtableを用いる
+# qiime feature-table filter-samples \
+# 	--p-where "[Species]='*'" \
+# 	--m-metadata-file /tmp/meta \
+# 	--i-table $BASIC/common_biology_free_table.qza  \
+# 	--o-filtered-table species_only_table.qza
+
+qiime composition add-pseudocount \
+	--i-table $BASIC/common_biology_free_table.qza  \
+	--o-composition-table count_added_table.qza
+
+qiime composition ancom \
+	--i-table count_added_table.qza \
+	--m-metadata-file /tmp/meta \
+	--m-metadata-column Species \
+	--o-visualization ancom-subject.qzv
