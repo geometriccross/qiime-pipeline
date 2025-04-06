7111764ee6a559ec5a10a312c9df5cd70932c273
geometriccross <geometriccross@desktop.>
2025-04-07 01:55:15 +0900

    機能を分離し、基本的な解析をbasicに移行

--- scripts/metagenome.sh
+++ scripts/metagenome.sh
@@ -2,7 +2,8 @@
 # 親スクリプトで定義された変数や関数を使用しているため、warningをdisableする
 # shellcheck disable=SC2154
 
-de "/scripts/pipeline/taxonomy.sh -s $SAMPLING_DEPTH"
+de "/scripts/pipeline/basic.sh -s $SAMPLING_DEPTH"
+de "/scripts/pipeline/taxonomy.sh"
 
 # /tmp/out/.のように最後に.をつけることで、指定したディレクトリの中身を直接コピーすることとなる
 docker cp "$batch_id":/tmp/out/. "out/$batch_id/"
