32df29e1a7603a07dbcfe1679309f927295d2fd9
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-15 13:48:49 +0900

    setup部分を作成

--- test/text_extract_id.py
+++ test/text_extract_id.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
 import unittest
+import shutil
+from pathlib import Path
+from datetime import time
 import scripts.extract_id
 
 
 class TestExtractID(unittest.TestCase):
     def setUp(self):
-        pass
+        hash_val = hash(str(time.time()).encode()).hexdigest()
+        test_dir = Path("/tmp") / hash_val
+        test_dir.mkdir()
+
+        for csv_file in Path("meta").glob("*.csv"):
+            shutil.copy(csv_file, test_dir)
+            test_dir = Path("/tmp").joinpath(hash(time))
+            test_dir.mkdir()
 
 
 if __name__ == "__main__":
