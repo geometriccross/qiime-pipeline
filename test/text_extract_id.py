e07bccbb66ac80d06aa593d519d73ecf63fe6299
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-15 13:37:32 +0900

    testを追加

---
+++ test/text_extract_id.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import unittest
+import scripts.extract_id
+
+
+class TestExtractID(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
