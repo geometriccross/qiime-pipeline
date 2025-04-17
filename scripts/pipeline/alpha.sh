dcf9a8e463af6ed6cff84550261d9f95b0366b28
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-17 14:11:18 +0900

    二つの組み合わせで解析を行うのを中止

--- scripts/pipeline/alpha.sh
+++ scripts/pipeline/alpha.sh
@@ -8,44 +8,42 @@ cd "$ALPHA" || exit 1
 set -ex
 pairs=(ctenocephalides_felis ischnopsyllus_needhami lipoptena_fortisetosa pedicinus_obtusus)
 for ((i = 0; i < ${#pairs[@]}; i++)); do
-	for ((j = i + 1; j < ${#pairs[@]}; j++)); do
-		combi=${pairs[i]}_${pairs[j]}
-		meta="/tmp/meta_$combi"
+	target=${pairs[i]}
+	meta="/tmp/meta_$target"
 
-		dst="$ALPHA/$combi"
-		mkdir -p $dst
-		cd $dst || exit 1
+	dst="$ALPHA/$target"
+	mkdir -p $dst
+	cd $dst || exit 1
 
-		python /scripts/extract_id.py /tmp/meta --column 3 "${pairs[i]}" "${pairs[j]}" >"$meta"
+	python /scripts/extract_id.py /tmp/meta --column 3 "${pairs[i]}" > "$meta"
 
-		qiime diversity filter-alpha-diversity \
-			--i-alpha-diversity "$CORE"/shannon_vector.qza \
-			--m-metadata-file "$meta" \
-			--o-filtered-alpha-diversity shannon_vector_"$combi".qza
+	qiime diversity filter-alpha-diversity \
+		--i-alpha-diversity "$CORE"/shannon_vector.qza \
+		--m-metadata-file "$meta" \
+		--o-filtered-alpha-diversity shannon_vector_"$target".qza
 
-		qiime diversity filter-alpha-diversity \
-			--i-alpha-diversity "$CORE"/faith_pd_vector.qza \
-			--m-metadata-file "$meta" \
-			--o-filtered-alpha-diversity faith_pd_vector_"$combi".qza
+	qiime diversity filter-alpha-diversity \
+		--i-alpha-diversity "$CORE"/faith_pd_vector.qza \
+		--m-metadata-file "$meta" \
+		--o-filtered-alpha-diversity faith_pd_vector_"$target".qza
 
-		qiime diversity filter-alpha-diversity \
-			--i-alpha-diversity "$CORE"/observed_features_vector.qza \
-			--m-metadata-file "$meta" \
-			--o-filtered-alpha-diversity observed_features_vector_"$combi".qza
+	qiime diversity filter-alpha-diversity \
+		--i-alpha-diversity "$CORE"/observed_features_vector.qza \
+		--m-metadata-file "$meta" \
+		--o-filtered-alpha-diversity observed_features_vector_"$target".qza
 
-		qiime diversity alpha-group-significance \
-			--m-metadata-file "$meta" \
-			--i-alpha-diversity shannon_vector_"$combi".qza \
-			--o-visualization shannon_"$combi".qzv
+	qiime diversity alpha-group-significance \
+		--m-metadata-file "$meta" \
+		--i-alpha-diversity shannon_vector_"$target".qza \
+		--o-visualization shannon_"$target".qzv
 
-		qiime diversity alpha-group-significance \
-			--m-metadata-file "$meta" \
-			--i-alpha-diversity faith_pd_vector_"$combi".qza \
-			--o-visualization faith_pd_"$combi".qzv
+	qiime diversity alpha-group-significance \
+		--m-metadata-file "$meta" \
+		--i-alpha-diversity faith_pd_vector_"$target".qza \
+		--o-visualization faith_pd_"$target".qzv
 
-		qiime diversity alpha-group-significance \
-			--m-metadata-file "$meta" \
-			--i-alpha-diversity observed_features_vector_"$combi".qza \
-			--o-visualization observed_features_"$combi".qzv
-	done
+	qiime diversity alpha-group-significance \
+		--m-metadata-file "$meta" \
+		--i-alpha-diversity observed_features_vector_"$target".qza \
+		--o-visualization observed_features_"$target".qzv
 done
