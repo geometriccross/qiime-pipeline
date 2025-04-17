f19247d0446218fb4306c4be6ce4fb7752da279b
geometriccross <geometriccross@LAPTOP.localdomain>
2025-04-17 10:56:41 +0900

    サンプルをフィルターする処理を追加

--- scripts/pipeline/alpha.sh
+++ scripts/pipeline/alpha.sh
@@ -14,19 +14,34 @@ for ((i = 0; i < ${#pairs[@]}; i++)); do
 
 		python /scripts/extract_id.py /tmp/meta --column 3 "${pairs[i]}" "${pairs[j]}" >"$meta"
 
+		qiime diversity filter-alpha-diversity \
+			--i-alpha-diversity "$CORE"/shannon_vector.qza \
+			--m-metadata-file "$meta" \
+			--o-filtered-alpha-diversity shannon_vector_"$combi".qza
+
+		qiime diversity filter-alpha-diversity \
+			--i-alpha-diversity "$CORE"/faith_pd_vector.qza \
+			--m-metadata-file "$meta" \
+			--o-filtered-alpha-diversity faith_pd_vector_"$combi".qza
+
+		qiime diversity filter-alpha-diversity \
+			--i-alpha-diversity "$CORE"/observed_features_vector.qza \
+			--m-metadata-file "$meta" \
+			--o-filtered-alpha-diversity observed_features_vector_"$combi".qza
+
 		qiime diversity alpha-group-significance \
 			--m-metadata-file "$meta" \
-			--i-alpha-diversity "$CORE"/shannon_vector.qza \
+			--i-alpha-diversity shannon_vector_"$combi".qza \
 			--o-visualization shannon_"$combi".qzv
 
 		qiime diversity alpha-group-significance \
 			--m-metadata-file "$meta" \
-			--i-alpha-diversity "$CORE"/faith_pd_vector.qza \
+			--i-alpha-diversity faith_pd_vector_"$combi".qza \
 			--o-visualization faith_pd_"$combi".qzv
 
 		qiime diversity alpha-group-significance \
 			--m-metadata-file "$meta" \
-			--i-alpha-diversity "$CORE"/observed_features_vector.qza \
+			--i-alpha-diversity observed_features_vector_"$combi".qza \
 			--o-visualization observed_features_"$combi".qzv
 	done
 done
