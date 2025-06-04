585c296edf5538dcda659bd993334c8ca2bbae0d
geometriccross <geometriccross@desktop.>
2025-06-05 01:39:46 +0900

    Datasetクラスの属性名をfastq_pathからfastq_folderに変更し、関連するテストを更新

--- scripts/data_control/dataset.py
+++ scripts/data_control/dataset.py
@@ -9,12 +9,12 @@ class Dataset:
     """
 
     name: str
-    fastq_path: Path
+    fastq_folder: Path
     metadata_path: Path
 
     def __post_init__(self):
-        if not self.fastq_path.exists():
-            raise FileNotFoundError(f"Fastq path {self.fastq_path} does not exist.")
+        if not self.fastq_folder.exists():
+            raise FileNotFoundError(f"Fastq path {self.fastq_folder} does not exist.")
         if not self.metadata_path.exists():
             raise FileNotFoundError(
                 f"Metadata path {self.metadata_path} does not exist."
--- test/data_control/test_dataset.py
+++ test/data_control/test_dataset.py
@@ -1,28 +1,28 @@
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from scripts.data_control.dataset import Dataset
+from scripts.data_control.dataset import Dataset, Databank
 
 
 def test_dataset_raise_error_when_specify_incorrect_path():
-    def check(fastq_path, metadata_path, err_msg):
+    def check(fastq_folder, metadata_path, err_msg):
         try:
             Dataset(
                 name="test_dataset",
-                fastq_path=fastq_path,
+                fastq_folder=fastq_folder,
                 metadata_path=metadata_path,
             )
         except FileNotFoundError as e:
             assert str(e) == err_msg
 
     check(
-        fastq_path=Path("non_existent.fastq"),
+        fastq_folder=Path("non_existent.fastq"),
         metadata_path=Path("non_existent_metadata.tsv"),
         err_msg="Fastq path non_existent.fastq does not exist.",
     )
 
     with NamedTemporaryFile(delete=True) as fastq_file:
         check(
-            fastq_path=Path(fastq_file.name),
+            fastq_folder=Path(fastq_file.name),
             metadata_path=Path("non_existent_metadata.tsv"),
             err_msg="Metadata path non_existent_metadata.tsv does not exist.",
         )
@@ -30,6 +30,6 @@ def test_dataset_raise_error_when_specify_incorrect_path():
         with NamedTemporaryFile(delete=True) as metadata_file:
             assert Dataset(
                 name="successful_dataset",
-                fastq_path=Path(fastq_file.name),
+                fastq_folder=Path(fastq_file.name),
                 metadata_path=Path(metadata_file.name),
             )
