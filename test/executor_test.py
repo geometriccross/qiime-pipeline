8e98fd6ab3852a7f90e6046a1e6e67a964f8ac93
geometriccross <geometriccross@desktop.>
2025-06-04 23:06:43 +0900

    テストを追加

--- test/executor_test.py
+++ test/executor_test.py
@@ -8,3 +8,12 @@ def test_executor_create_instance(container: docker.models.containers.Container)
     executor = Executor(ctn)
     assert isinstance(executor, Executor)
     assert executor.status() == "created"
+
+
+def test_executor_start_and_stop(container: docker.models.containers.Container):
+    ctn = container("alpine")
+
+    with Executor(ctn) as executor:
+        assert executor.status() == "running"
+
+    assert executor.status() == "removing"
