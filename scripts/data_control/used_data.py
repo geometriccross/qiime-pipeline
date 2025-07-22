f2bc88775284442962aac0002fc2562b2f1c3bd4
geometriccross <geometriccross@LAPTOP.localdomain>
2025-07-22 17:50:25 +0900

    regionを追加

--- scripts/data_control/used_data.py
+++ scripts/data_control/used_data.py
@@ -2,6 +2,7 @@ import docker
 from pathlib import Path
 from .dataset import Dataset, Databank
 from .container import provid_container, mounts
+from .ribosome_regions import V3V4
 
 
 def used_data(
@@ -11,24 +12,28 @@ def used_data(
         name="bat_fleas",
         fastq_folder=container_side_fastq_stored_path.joinpath("/batfleas"),
         metadata_path=container_side_meta_stored_path.joinpath("/bat_fleas.csv"),
+        region=V3V4,
     )
 
     __cat_fleas = Dataset(
         name="cat_fleas",
         fastq_folder=container_side_fastq_stored_path.joinpath("/catfleas"),
         metadata_path=container_side_meta_stored_path.joinpath("/cat_fleas.csv"),
+        region=V3V4,
     )
 
     __deer_keds = Dataset(
         name="deer_keds",
         fastq_folder=container_side_fastq_stored_path.joinpath("/deerkeds"),
         metadata_path=container_side_meta_stored_path.joinpath("/deer_keds.csv"),
+        region=V3V4,
     )
 
     __mk_louses = Dataset(
         name="mk_louses",
         fastq_folder=container_side_fastq_stored_path.joinpath("/mklouses"),
         metadata_path=container_side_meta_stored_path.joinpath("/mk_louses.csv"),
+        region=V3V4,
     )
 
     return Databank(
