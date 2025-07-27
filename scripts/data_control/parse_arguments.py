6945225b8a595106f06df276661eafad7870d433
geometriccross <geometriccross@LAPTOP.localdomain>
2025-07-27 16:37:13 +0900

    使用するデータを指定する引数を作成

---
+++ scripts/data_control/parse_arguments.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import argparse
+
+
+def parse_pair(pair_str: str) -> list[tuple[Path, Path]]:
+    """Parse a string of the form 'metadata_path:fastq_folder' into a tuple of Paths."""
+    result = []
+    for pair in pair_str.split(","):
+        if ":" not in pair:
+            raise ValueError(
+                f"Invalid pair format: {pair}. Expected 'metadata_path:fastq_folder'."
+            )
+
+        metadata_path_str, fastq_folder_str = pair.split(":")
+
+        if not metadata_path_str or not fastq_folder_str:
+            raise ValueError(
+                f"""Invalid pair format: {pair}.
+                    Both metadata path and fastq folder must be provided."""
+            )
+
+        result.append(
+            (
+                Path(metadata_path_str.strip()),
+                Path(fastq_folder_str.strip()),
+            )
+        )
+
+        return result
+
+
+def argument_parser():
+    """Create and return an argument parser for the QIIME pipeline."""
+    parser = argparse.ArgumentParser(description="Run the QIIME pipeline.")
+    parser.add_argument(
+        "--data",
+        type=list[tuple[Path, Path]],
+        nargs="+",
+        help="""
+            Pairs of fastq and metadata paths to use for the pipeline.
+            Example:
+                --data path/to/metadata_path:path/to/fastq_folder,
+                       path/to/another_metadata:path/to/another_fastq_folder
+            """,
+    )
+    return parser
---
+++ test/data_control/parse_arguments_test.py
@@ -0,0 +1,43 @@
+import pytest
+from pathlib import Path
+from scripts.data_control.parse_arguments import parse_pair
+
+
+@pytest.mark.parametrize(
+    "pair_str,expected",
+    [
+        (
+            "metadata_path:fastq_folder",
+            [(Path("metadata_path"), Path("fastq_folder"))],
+        ),
+        (
+            "  metadata_path  :  fastq_folder  ",
+            [(Path("metadata_path"), Path("fastq_folder"))],
+        ),
+    ],
+)
+def test_parse_pair(pair_str, expected):
+    """Test the parse_pair function."""
+    assert parse_pair(pair_str) == expected
+
+
+def test_parse_pair_if_give_multiple_pairs():
+    """Test if multiple pairs are parsed correctly."""
+    pair_str = "metadata1:fastq1,metadata2:fastq2"
+    expected = [
+        (Path("metadata1"), Path("fastq1")),
+        (Path("metadata2"), Path("fastq2")),
+    ]
+    assert parse_pair(pair_str)[0] == expected[0]
+
+
+def test_parse_pair_invalid_format():
+    """Test if ValueError is raised for invalid pair format."""
+    with pytest.raises(ValueError, match="Invalid pair format: invalid_pair"):
+        parse_pair("invalid_pair")
+
+    with pytest.raises(ValueError, match="Invalid pair format: metadata_path"):
+        parse_pair("metadata_path")
+
+    with pytest.raises(ValueError, match="Invalid pair format: :fastq_folder"):
+        parse_pair(":fastq_folder")
