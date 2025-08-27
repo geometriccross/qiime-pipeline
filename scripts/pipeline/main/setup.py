26302da1fa2de90e8e9f46bffa38fe95886c945e
geometriccross <geometriccross@desktop.>
2025-08-28 00:32:19 +0900

    pipeline/support/setup.pyの内容をpipeline/main/setup.pyへ移植。それに伴いtestも移植

--- scripts/pipeline/main/setup.py
+++ scripts/pipeline/main/setup.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 from typing import Tuple
+from argparse import Namespace
 from scripts.data.control.check_manifest import check_manifest
 from scripts.data.control.create_Mfiles import create_Mfiles
-from scripts.data.store.dataset import Datasets
+from scripts.data.store.dataset import Datasets, Dataset
+from scripts.data.store.setting_data_structure import SettingData
+from scripts.data.store.ribosome_regions import V3V4
+from scripts.pipeline.support.executor import Executor, Provider
+from scripts.pipeline.support.parse_arguments import argument_parser
+from typing import Generator
 
 
 def setup_files(output: Path, datasets: Datasets) -> Tuple[Path, Path]:
@@ -17,3 +23,40 @@ def setup_files(output: Path, datasets: Datasets) -> Tuple[Path, Path]:
         raise ValueError("Manifest file is invalid")
 
     return Path(metafile), Path(manifest)
+
+
+def setup_datasets(arg: Namespace) -> Datasets:
+    data = []
+    for metadata_path, fastq_folder in arg.data:
+        # Use the basename of the metadata path as the dataset name
+        data.append(
+            Dataset(
+                name=fastq_folder.stem,
+                fastq_folder=fastq_folder,
+                metadata_path=metadata_path,
+                region=V3V4(),
+            )
+        )
+
+    return Datasets(sets=set(data))
+
+
+def setup() -> Generator[tuple[SettingData, Executor], None, None]:
+    args = argument_parser().parse_args()
+    setting_data = SettingData(
+        workspace_path=args.workspace,
+        dockerfile=args.dockerfile,
+        datasets=setup_datasets(args),
+    )
+
+    # 設定データを保存
+    setting_data.write(args.output / "settings.toml")
+
+    provider = Provider.from_dockerfile(
+        setting_data.dockerfile,
+        mounts=setting_data.datasets.mounts,
+        workspace=setting_data.workspace_path,
+        remove=True,
+    )
+
+    yield setting_data, Executor(provider.provide())
--- test/pipeline/main/setup_test.py
+++ test/pipeline/main/setup_test.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from scripts.pipeline.main.setup import setup_files
+from scripts.pipeline.main.setup import setup_files, setup_datasets
 from scripts.data.store.dataset import Datasets
 
 
@@ -16,3 +16,15 @@ def test_setup_files_currently_created(dummy_datasets: Datasets):
                 csv = line.split("\t")
                 assert csv[0] == f"id{i}"
                 assert csv[1] == f"test{i}"
+
+
+def test_setup_datasets(namespace):
+    datasets = setup_datasets(namespace)
+    assert datasets is not None
+    assert len(datasets.sets) == 4  # data_path_pairで生成したものが4つのため
+
+    for datasets in datasets.sets:
+        assert datasets.name in ["test1", "test2", "test3", "test4"]
+        assert datasets.fastq_folder.exists()
+        assert datasets.metadata_path.exists()
+        assert datasets.region is not None
