f33fb5246a9db8610754c9df74a31179017c0b2e
geometriccross <geometriccross@LAPTOP.localdomain>
2025-09-03 08:57:47 +0900

    parser部を削除

--- scripts/data/control/create_Mfiles.py
+++ scripts/data/control/create_Mfiles.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 import csv
-import argparse
-from textwrap import dedent
 from pathlib import Path, PurePath
 from scripts.data.store.dataset import Datasets
-from scripts.data.store.used_data import used_data
 from .validate_pattern import Direction, check_current_pair, extract_first_underscore
 
 
@@ -114,56 +111,3 @@ def create_Mfiles(
     with open(out_mani, "w") as f:
         writer = csv.writer(f, delimiter="\t")
         writer.writerows(manifest)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-p",
-        "--id-prefix",
-        default="id",
-        help=dedent(
-            """
-            set prefix id of each sample like this [REPLACE THIS] + numeric
-            (default : %(default)s))
-            """
-        ).strip(),
-    )
-    parser.add_argument("--input-meta", default="/meta")
-    parser.add_argument("--input-fastq", default="/fastq")
-    parser.add_argument(
-        "--out-meta",
-        help=dedent(
-            """
-            output path to metadata
-            """
-        ).strip(),
-    )
-    parser.add_argument(
-        "--out-mani",
-        help=dedent(
-            """
-            output path to manifest file
-            """
-        ).strip(),
-    )
-    parser.add_argument(
-        "--meta-fastq-pair",
-        help=dedent(
-            """
-            set a DICTIONARY metadata and fastq file pathes as a pair.
-            e.g. --meta-fastq-pair {meta: /foo/bar/metafile.csv,/hoge/huga/fastqdir}
-            """
-        ).strip(),
-    )
-
-    # 引数から取得
-    args = parser.parse_args()
-    id_prefix = args.id_prefix
-    input_meta = args.input_meta
-    input_fastq = args.input_fastq
-    out_meta = args.out_meta
-    out_mani = args.out_mani
-    # 引数のdefaultで指定してないのは、その段階ではまだ割り当てられていないinput_metaを使いたかったから
-
-    create_Mfiles(id_prefix, out_meta, out_mani, used_data())
