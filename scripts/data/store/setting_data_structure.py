7b475ccba84cf13a701fd9da369e378261715ebc
geometriccross <geometriccross@LAPTOP.localdomain>
2025-09-02 21:47:52 +0900

    SettingDataに羅列して書かれていたものを細分化し、新しい設定データ保存用クラスを作成

--- scripts/data/store/setting_data_structure.py
+++ scripts/data/store/setting_data_structure.py
@@ -7,17 +7,30 @@ from .dataset import Datasets
 from .generate_id import generate_id
 
 
+class PairPath:
+    """
+    ローカルのパスとDockerコンテナでのマウント先の組
+    """
+
+    def __init__(self, local_pos: Path, ctn_pos: Path):
+        self.local_pos = local_pos
+        self.ctn_pos = ctn_pos
+
+
+@dataclasses.dataclass
+class ContainerData:
+    image_or_dockerfile: str | Path
+    workspace_path: Path
+    output_path: PairPath
+    database_path: PairPath
+
+
 @dataclasses.dataclass
 class SettingData(Mapping):
-    dockerfile: Path
+    container_data: ContainerData
 
     datasets: Datasets
-    database: Path
-    sampling_depth: int
-
-    workspace_path: Path = dataclasses.field(default_factory=lambda: Path("workspace"))
-    output_path: Path = dataclasses.field(default_factory=lambda: Path("output"))
-
+    sampling_depth: int = 10000
     batch_id: str = dataclasses.field(default_factory=generate_id)
 
     def __post_init__(self):
