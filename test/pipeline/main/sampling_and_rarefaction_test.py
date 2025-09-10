d3d21dd37d7af220ea3fed83aa367f577b414007
geometriccross <geometriccross@desktop.>
2025-09-10 20:16:10 +0900

    テストを簡略化。使用しないmockを削除

--- test/pipeline/main/sampling_and_rarefaction_test.py
+++ test/pipeline/main/sampling_and_rarefaction_test.py
@@ -1,23 +1,9 @@
-from pathlib import Path
-from scripts.data.store import SettingData
-from scripts.pipeline.support import Executor, Provider
+from scripts.pipeline.main.setup import setup_context
 from scripts.pipeline.main.sampling_and_rarefaction import (
     run_rarefaction,
-    copy_from_container,
-    execute,
 )
 
 
-def test_run_rarefaction(setting: SettingData):
-    provider = Provider(
-        image="quay.io/qiime2/amplicon:2024.10",
-        mounts=setting.datasets.mounts(Path("/data")),
-        workspace=setting.workspace_path,
-        remove=True,
-    )
-
-    executor = Executor(provider.provide())
-    run_rarefaction(
-        setting=setting,
-        executor=executor,
-    )
+def test_run_rarefaction(namespace):
+    context = setup_context(namespace)
+    run_rarefaction(context)
