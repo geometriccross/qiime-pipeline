1c4f7f6ceef83cbba8234c7f127a58c6790dc718
geometriccross <geometriccross@LAPTOP.localdomain>
2025-09-12 17:20:38 +0900

    pytest.mark.slowを追加

--- test/pipeline/main/sampling_and_rarefaction_test.py
+++ test/pipeline/main/sampling_and_rarefaction_test.py
@@ -1,9 +1,11 @@
+import pytest
 from scripts.pipeline.main.setup import setup_context
 from scripts.pipeline.main.sampling_and_rarefaction import (
     run_rarefaction,
 )
 
 
+@pytest.mark.slow
 def test_run_rarefaction(namespace):
     context = setup_context(namespace)
     run_rarefaction(context)
