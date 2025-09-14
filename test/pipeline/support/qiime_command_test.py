4e807cc4b91684966ad37140f91182964ac0cfdc
geometriccross <geometriccross@LAPTOP.localdomain>
2025-09-14 16:26:44 +0900

    不要なテストを削除し、string化のテストを追加

--- test/pipeline/support/qiime_command_test.py
+++ test/pipeline/support/qiime_command_test.py
@@ -1,7 +1,6 @@
-import pytest
 from scripts.pipeline.support import Q2CmdAssembly
 
 
-def test_Q2CmdAssembly_instance_concatinate_with_incorrect_base_command():
-    with pytest.raises(ValueError):
-        Q2CmdAssembly("qiime tools import") + Q2CmdAssembly("qiime tools export")
+def test_Q2CmdAssembly_convert_to_str():
+    assembly = Q2CmdAssembly("qiime tools import")
+    assert str(assembly) == "qiime tools import"
