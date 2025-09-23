488c040e7a6a14f0386200a641d71e51e0c85de3
geometriccross <geometriccross@LAPTOP.localdomain>
2025-09-23 16:33:11 +0900

    qiime_commandモジュールを作成

--- scripts/pipeline/support/__init__.py
+++ scripts/pipeline/support/__init__.py
@@ -1,4 +1,8 @@
 from .executor import Executor, Provider
 from .parse_arguments import argument_parser
-from .qiime_command import Q2CmdAssembly, CircularDependencyError, IsolatedCommandError
+from .qiime_command.qiime_command import (
+    Q2CmdAssembly,
+    CircularDependencyError,
+    IsolatedCommandError,
+)
 from .view import QzvViewer
