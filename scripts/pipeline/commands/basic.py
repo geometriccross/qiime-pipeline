bb2d07a78b9f6866869cc85d42f7f308478ffadc
geometriccross <geometriccross@desktop.>
2025-09-24 20:34:26 +0900

    複数ファイルにまとめていたスクリプトを一本化

--- scripts/pipeline/commands/basic.py
+++ scripts/pipeline/commands/basic.py
@@ -10,16 +10,18 @@ class basic_pipeline(support.Pipeline):
 
     def command_list(self) -> tuple[support.Q2CmdAssembly, str, list[str]]:
         output = self.__context.setting.container_data.output_path.ctn_pos
+
         pre_dir = output / "pre"
         core_dir = output / "core"
+        alpha_dir = output / "alpha"
+        beta_dir = output / "beta"
 
         requires = support.RequiresDirectory()
         requires.add(output)
         requires.add(pre_dir)
         requires.add(core_dir)
 
-        # fmt: off
-
+        # region Preprocessing
         assembly = support.Q2CmdAssembly()
         imported = (
             assembly.new_cmd("qiime tools import")
@@ -103,8 +105,13 @@ class basic_pipeline(support.Pipeline):
             assembly.new_cmd("qiime phylogeny align-to-tree-mafft-fasttree")
             .add_option("quiet")
             .add_input("sequences", bio_free_seq)
-            .add_output("alignment", output / "common_biology_free_aligned-rep-seqs.qza")
-            .add_output("masked-alignment", output / "common_biology_free_masked-aligned-rep-seqs.qza")
+            .add_output(
+                "alignment", output / "common_biology_free_aligned-rep-seqs.qza"
+            )
+            .add_output(
+                "masked-alignment",
+                output / "common_biology_free_masked-aligned-rep-seqs.qza",
+            )
             .add_output("tree", output / "common_biology_free_unrooted-tree.qza")
             .add_output("rooted-tree", output / "common_biology_free_rooted-tree.qza")
             .get_outputs()
@@ -115,30 +122,12 @@ class basic_pipeline(support.Pipeline):
             .add_option("quiet")
             .add_input("classifier", db_path)
             .add_input("reads", bio_free_seq)
-            .add_output("classification", output / "common_biology_free_classification.qza")
+            .add_output(
+                "classification", output / "common_biology_free_classification.qza"
+            )
             .get_outputs()
         )
-
-
-
-        # core-metrics-phylogenetic requires the following outputs:
-        #  --o-rarefied-table
-        #  --o-faith-pd-vector
-        #  --o-observed-features-vector
-        #  --o-shannon-vector
-        #  --o-evenness-vector
-        #  --o-unweighted-unifrac-distance-matrix
-        #  --o-weighted-unifrac-distance-matrix
-        #  --o-jaccard-distance-matrix
-        #  --o-bray-curtis-distance-matrix
-        #  --o-unweighted-unifrac-pcoa-results
-        #  --o-weighted-unifrac-pcoa-results
-        #  --o-jaccard-pcoa-results
-        #  --o-bray-curtis-pcoa-results
-        #  --o-unweighted-unifrac-emperor
-        #  --o-weighted-unifrac-emperor
-        #  --o-jaccard-emperor
-        #  --o-bray-curtis-emperor
+        # endregion
 
         core_metrics_files = (
             assembly.new_cmd("qiime diversity core-metrics-phylogenetic")
@@ -149,25 +138,106 @@ class basic_pipeline(support.Pipeline):
             .add_parameter("sampling-depth", str(sampling_depth))
             .add_output("rarefied-table", core_dir / "rarefied_table.qza")
             .add_output("faith-pd-vector", core_dir / "faith_pd_vector.qza")
-            .add_output("observed-features-vector", core_dir / "observed_features_vector.qza")
+            .add_output(
+                "observed-features-vector", core_dir / "observed_features_vector.qza"
+            )
             .add_output("shannon-vector", core_dir / "shannon_vector.qza")
             .add_output("evenness-vector", core_dir / "evenness_vector.qza")
-            .add_output("unweighted-unifrac-distance-matrix", core_dir / "unweighted_unifrac_distance_matrix.qza")
-            .add_output("weighted-unifrac-distance-matrix", core_dir / "weighted_unifrac_distance_matrix.qza")
-            .add_output("jaccard-distance-matrix", core_dir / "jaccard_distance_matrix.qza")
-            .add_output("bray-curtis-distance-matrix", core_dir / "bray_curtis_distance_matrix.qza")
-            .add_output("unweighted-unifrac-pcoa-results", core_dir / "unweighted_unifrac_pcoa_results.qza")
-            .add_output("weighted-unifrac-pcoa-results", core_dir / "weighted_unifrac_pcoa_results.qza")
+            .add_output(
+                "unweighted-unifrac-distance-matrix",
+                core_dir / "unweighted_unifrac_distance_matrix.qza",
+            )
+            .add_output(
+                "weighted-unifrac-distance-matrix",
+                core_dir / "weighted_unifrac_distance_matrix.qza",
+            )
+            .add_output(
+                "jaccard-distance-matrix", core_dir / "jaccard_distance_matrix.qza"
+            )
+            .add_output(
+                "bray-curtis-distance-matrix",
+                core_dir / "bray_curtis_distance_matrix.qza",
+            )
+            .add_output(
+                "unweighted-unifrac-pcoa-results",
+                core_dir / "unweighted_unifrac_pcoa_results.qza",
+            )
+            .add_output(
+                "weighted-unifrac-pcoa-results",
+                core_dir / "weighted_unifrac_pcoa_results.qza",
+            )
             .add_output("jaccard-pcoa-results", core_dir / "jaccard_pcoa_results.qza")
-            .add_output("bray-curtis-pcoa-results", core_dir / "bray_curtis_pcoa_results.qza")
-            .add_output("unweighted-unifrac-emperor", core_dir / "unweighted_unifrac_emperor.qzv")
-            .add_output("weighted-unifrac-emperor", core_dir / "weighted_unifrac_emperor.qzv")
+            .add_output(
+                "bray-curtis-pcoa-results", core_dir / "bray_curtis_pcoa_results.qza"
+            )
+            .add_output(
+                "unweighted-unifrac-emperor",
+                core_dir / "unweighted_unifrac_emperor.qzv",
+            )
+            .add_output(
+                "weighted-unifrac-emperor", core_dir / "weighted_unifrac_emperor.qzv"
+            )
             .add_output("jaccard-emperor", core_dir / "jaccard_emperor.qzv")
             .add_output("bray-curtis-emperor", core_dir / "bray_curtis_emperor.qzv")
             .get_outputs()
         )
+        # endregion
+
+        # region Alpha Diversity Analysis
+        for name, alpha_index in {
+            "observed": core_dir / "observed_features_vector.qza",
+            "shannon": core_dir / "shannon_vector.qza",
+            "faith_pd": core_dir / "faith_pd_vector.qza",
+        }.items():
+            filtered = (
+                assembly.new_cmd("qiime diversity filter-alpha-diversity")
+                .add_input("alpha-diversity", alpha_index)
+                .add_metadata("metadata-file", self.__context.ctn_metadata)
+                .add_output(
+                    "filtered-alpha-diversity", alpha_dir / f"filtered_{name}.qza"
+                )
+                .get_outputs()
+            )
+
+            alpha_visualized = (
+                assembly.new_cmd("qiime diversity alpha-group-significance")
+                .add_input("alpha-diversity", filtered)
+                .add_metadata("metadata-file", self.__context.ctn_metadata)
+                .add_output("visualization", filtered.replace(".qza", ".qzv"))
+                .get_outputs()
+            )
+        # endregion
+
+        # region Beta Diversity Analysis
+        for key in "Species", "Location", "SampleGender":
+            beta_visualized = (
+                assembly.new_cmd("qiime diversity beta-group-significance")
+                .add_option("quiet")
+                .add_parameter("p-value-correction", "fdr")
+                .add_parameter("pairwise", "--p-pairwise")
+                .add_metadata("metadata-file", self.__context.ctn_metadata)
+                .add_metadata("metadata-column", key)
+                .add_input(
+                    "distance-matrix", core_dir / "weighted_unifrac_distance_matrix.qza"
+                )
+                .add_output("visualization", beta_dir / f"weighted_unifrac-{key}.qzv")
+                .get_outputs()
+            )
+        # endregion
+
+        # region Taxonomy
+        taxonomy_visualized = (
+            assembly.new_cmd("qiime taxa barplot")
+            .add_option("quiet")
+            .add_input("table", bio_free_table)
+            .add_input("taxonomy", bio_free_classfied)
+            .add_metadata("metadata-file", self.__context.ctn_metadata)
+            .add_output("visualization", output / "taxa-bar-plots.qzv")
+            .get_outputs()
+        )
+        # endregion
 
         # fmt: on
 
         assembly.sort_commands()
-        return assembly, requires, bio_free_classfied, core_metrics_files
+        return assembly, requires
