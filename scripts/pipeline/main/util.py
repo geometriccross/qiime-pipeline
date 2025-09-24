f859418cfbf55b5ef623bd9d946a815e66df21de
geometriccross <geometriccross@desktop.>
2025-09-24 19:59:52 +0900

    utility関数を追加

--- scripts/pipeline/main/util.py
+++ scripts/pipeline/main/util.py
@@ -2,6 +2,12 @@ from pathlib import Path
 from .setup import PipelineContext
 
 
+def find(key: str, iterable: list) -> str:
+    for file in iterable:
+        if key in file:
+            return file
+
+
 def copy_from_container(context: PipelineContext, ctn_target_file: Path) -> Path:
     out_dir = Path(f"out/{context.setting.batch_id}")
     out_dir.mkdir(parents=True, exist_ok=True)
