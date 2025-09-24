cdfd3d780a3acd0ead13401e28d81661f94c9057
geometriccross <geometriccross@desktop.>
2025-09-24 20:36:09 +0900

    commands/__init__.pyを修正

--- scripts/pipeline/commands/__init__.py
+++ scripts/pipeline/commands/__init__.py
@@ -1,6 +1,3 @@
 from .alpha_rarefaction import alpha_rarefaction_pipeline
 from .db import db_pipeline
 from .basic import basic_pipeline
-from .alpha import alpha_analysis_pipeline
-from .beta import beta_analysis_pipeline
-from .taxonomy import taxonomy_pipeline
